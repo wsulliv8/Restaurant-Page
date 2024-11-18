6bd56038d121025f8646f463a6a455c59b72a337
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2024-11-17 16:22:53 -0800

    Change config files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,39 +1,7 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-	devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-			{ test: /\.html$/i,
-				loader: "html-loader",
-			},
-			{
-  			test: /\.(png|svg|jpg|jpeg|gif)$/i,
-  			type: "asset/resource",
-			}
-
-    ],
-  },
-};
-
+module.exports = merge(common, {
+  mode: 'production',
+	devtool: 'source-map',
+});
